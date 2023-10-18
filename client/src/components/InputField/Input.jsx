141d8342b02490a3b269f3f17742df8a7690aef2
sathiska sasindu <122218622+sathiska567@users.noreply.github.com>
2023-10-18 21:35:35 +0530

    added

--- client/src/components/InputField/Input.jsx
+++ client/src/components/InputField/Input.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function Input(props) {
+
+  const [name , setName] = useState('')
+  const [email , setEmail] = useState('')
+
+
   return (
     <div>
       <div className={`form-group`}>
         <div className={`col-sm-10`}>
-          <input type={props.type} className={`form-control`} name={props.name} placeholder={props.placeholder} required />
+          <input type={props.type} className={`form-control`} name={props.name} placeholder={props.placeholder} required  />
         </div>
       </div>
     </div>
